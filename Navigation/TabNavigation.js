0c899527658f224df202968ff49687ca04c4e434
Twlight_zone <twlightzonehelp@gmail.com>
2022-07-14 16:20:48 +0530

    Doing cool stuff

--- Navigation/TabNavigation.js
+++ Navigation/TabNavigation.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import Ionicon from 'react-native-vector-icons/Ionicons';
 
 import Feed from '../Screens/Feed';
 import CreatePost from '../Screens/CreatPost';
+import { StyleSheet } from 'react-native';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const BottomTabNavigator = () => {
     return(
         <Tab.Navigator
+            labeled={false}
+            barStyle={styles.bottomTabStyle}
             screenOptions = {({route})=>({
                 tabBarIcon: ({ focused, color, size }) => {
                     let iconName;
@@ -31,5 +34,16 @@ const BottomTabNavigator = () => {
         </Tab.Navigator>
     );
 }
+
+const styles = StyleSheet.create({
+    bottomTabStyle : {
+        backgroundColor: "#2f345d",
+        height: "8%",
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        overflow: "hidden",
+        position: "absolute"
+    }
+})
 
 export default BottomTabNavigator;
